2ce05e5411b652b90d75890eebd414754b0f1db4
zenaldo-oliveira <zenaldo_18@hotmail.com>
2024-03-28 00:52:41 -0400

    atualização do codigo

--- scripts.js
+++ scripts.js
@@ -1,4 +1,18 @@
-function cliqueiNoBotao(){
-    
-    alert("Você clicou no botão")
+const form = document.querySelector(".fale-conosco");
+const mascara = document.querySelector(".mascara-formulario");
+
+
+
+function mostrarForm(){
+    form.style.left = "50%"
+    form.style.transform =  "translateX(-50%)"
+    mascara.style.visibility = "visible"
+
+}
+
+function escoderForm(){
+    form.style.left = "-300px"
+    form.style.transform =  "translateX(0)"
+    mascara.style.visibility = "hidden"
+
 }
